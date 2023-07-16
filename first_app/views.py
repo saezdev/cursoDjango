1c736b5b4e8a3c6a7ffc977f1b98ae70db7d1521
Sergio <sergiosaezdev@gmail.com>
2023-07-16 05:35:08 +0200

    Creacion de primer Template

--- first_app/views.py
+++ first_app/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Hola Mundo')
+    my_dic = {'insert_me':"Hello i am from first_app/index.html!"}
+    return render(request, 'first_app/index.html', context=my_dic)
--- first_project/settings.py
+++ first_project/settings.py
@@ -11,9 +11,13 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+#De esta forma se podra acceder al directorio desde cualquier sistema operativo de forma automatica
+TEMPLATE_DIR = os.path.join(BASE_DIR,"templates")
 
 
 # Quick-start development settings - unsuitable for production
@@ -54,7 +58,7 @@ ROOT_URLCONF = "first_project.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [TEMPLATE_DIR,],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
