8279fde7c9b1c94ab57920f16932d6bd17b8b01c
Sergio <sergiosaezdev@gmail.com>
2023-07-16 05:07:28 +0200

    URL Mappings

--- first_project/urls.py
+++ first_project/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from django.conf.urls import include
 from first_app import views
 
 urlpatterns = [
+    re_path(r'^first_app/', include('first_app.urls')),
     path("", views.index, name="index"),
     path("admin/", admin.site.urls),
 ]
