3e54a8501fff198343cf7322050bbc2467b10af0
Sergio <sergiosaezdev@gmail.com>
2023-07-24 03:35:55 +0200

    Form Validation - custom validators - email verification

--- first_app/forms.py
+++ first_app/forms.py
@@ -4,12 +4,25 @@ from django.core import validators
 def check_for_z(value):
     if value[0].lower() != 'z':
         raise forms.ValidationError('NAME NEEDS TO START WITH Z')
+    
+
 
 class FormName(forms.Form):
     name = forms.CharField(validators=[check_for_z])
     email = forms.EmailField()
+    verify_email = forms.EmailField(label='Enter your email again')
     text = forms.CharField(widget=forms.Textarea)
     botcatcher = forms.CharField(required=False, widget=forms.HiddenInput, validators=[validators.MaxLengthValidator(0)])
+    
+    def clean(self):
+        all_cleaned_data = super().clean()
+        email = all_cleaned_data['email']
+        vemail = all_cleaned_data['verify_email']
+        
+        if email != vemail:
+            raise forms.ValidationError("MAKE SURE EMAILS MATCH!")
+    
+    
     
         
 
