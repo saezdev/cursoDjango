cd5efc536b51f85a645a3cd324e8e7c69d5715dd
Sergio <sergiosaezdev@gmail.com>
2023-07-24 02:15:21 +0200

    Form basics

--- first_app/urls.py
+++ first_app/urls.py
@@ -1,6 +1,7 @@
-from django.urls import re_path
+from django.urls import re_path, path
 from first_app import views
 
 urlpatterns = [
     re_path(r'^$', views.index, name = 'index'),
+    path('forms/', views.form_name)
 ]
--- first_app/views.py
+++ first_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from first_app.models import Topic, Webpage, AccessRecord
+from . import forms
 
 # Create your views here.
 
@@ -9,3 +10,18 @@ def index(request):
     date_dict = {'access_records': webpages_list}
     my_dic = {'insert_me':"Hello i am from first_app/index.html!"}
     return render(request, 'first_app/index.html', context=date_dict)
+
+def form_name(request):
+    form = forms.FormName()
+    
+    if request.method == 'POST':
+        form = forms.FormName(request.POST)
+        
+        if form.is_valid():
+            #DO SOMETHING HERE
+            print('VALIDATION SUCCESS!')
+            print("Name: " + form.cleaned_data['name'])
+            print("Email: " + form.cleaned_data['email'])
+            print("Text: " + form.cleaned_data['text'])
+            
+    return render(request, 'first_app/forms.html', {'form': form})
