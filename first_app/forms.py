ad4da704de85336f712c5ad901c3fdd78524f57b
Sergio <sergiosaezdev@gmail.com>
2023-07-24 03:24:08 +0200

    Form Validation - django built-in validators

--- first_app/forms.py
+++ first_app/forms.py
@@ -1,16 +1,11 @@
 from django import forms
+from django.core import validators
 
 class FormName(forms.Form):
     name = forms.CharField()
     email = forms.EmailField()
     text = forms.CharField(widget=forms.Textarea)
-    botcatcher = forms.CharField(required=False, widget=forms.HiddenInput)
+    botcatcher = forms.CharField(required=False, widget=forms.HiddenInput, validators=[validators.MaxLengthValidator(0)])
     
-    def clean_botcatcher(self):
-        botcatcher = self.cleaned_data['botcatcher']
-        
-        if len(botcatcher) > 0:
-            raise forms.ValidationError("GOTCHA BOT!")
-        return botcatcher
         
 
