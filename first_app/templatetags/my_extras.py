6f64aba5a4e2b545f2bec5cbad55d4c88a2d1e18
Sergio <sergiosaezdev@gmail.com>
2023-07-26 02:57:18 +0200

    How to register a custom filter using decorators

--- first_app/templatetags/my_extras.py
+++ first_app/templatetags/my_extras.py
@@ -2,12 +2,10 @@ from django import template
 
 register = template.Library()
 
-
+@register.filter(name='cut')
 def cut(value,arg):
     """
     This cuts off all values of "arg" from the string!
     """
     
     return value.replace(arg,'')
-
-register.filter('cut',cut)
